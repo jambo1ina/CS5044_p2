5f41f15fac7033001b5d4ecf75d149e0e11ccf78
Jamie Munro <jbm@jbm.fyi>
2022-03-26 14:48:17 +0000

    Testing git lfs

--- extract_data.py
+++ extract_data.py
@@ -1,12 +1,13 @@
 import csv
 
 INPUT_CSV = ["ess/round1.csv", "ess/round2.csv", "ess/round3.csv", "ess/round4.csv", "ess/round5.csv", "ess/round6.csv", "ess/round7.csv", "ess/round8.csv", "ess/round9.csv"]
-OUTPUT_CSV = "data.csv"
+OUTPUT_CSV = "ess.csv"
 DELIMITER = ","
 VARIABLES_CSV = "variables.csv"
 VARIABLE_NAME_INDEX = 0
 MISSING_DATA = "-"
 
+#get variable names from input csv
 variableNames = []
 with open(VARIABLES_CSV, "r") as f:
     reader = csv.reader(f, delimiter=DELIMITER)
@@ -16,11 +17,32 @@ with open(VARIABLES_CSV, "r") as f:
         else:
             variableNames.append(line[VARIABLE_NAME_INDEX])
 
-print("extracting: ", variableNames)
+#expand all wildcard variable names
+expandedVariablesNames = []
+for variable in variableNames:
+    if "*" in variable:
+        prefix = variable[:variable.find("*")]
+        
+        for input in INPUT_CSV:
+            with open(input, "r") as f:
+               reader = csv.reader(f, delimiter=DELIMITER)
+               for i, line in enumerate(reader):
+                    if i != 0:
+                       break
+                    else:
+                        for heading in line:
+                            if heading.startswith(prefix):
+                                expandedVariablesNames.append(heading)
+
+    else:
+        expandedVariablesNames.append(variable)
+
+print("extracting: ", expandedVariablesNames)
 print("to file: ", OUTPUT_CSV)
 
+#genereate output CSV
 with open(OUTPUT_CSV, 'w') as out:
-    for heading in variableNames:
+    for heading in expandedVariablesNames:
         out.write(heading)
         out.write(",")
 
@@ -30,15 +52,17 @@ with open(OUTPUT_CSV, 'w') as out:
         with open(file, "r") as f:
             reader = csv.reader(f, delimiter=DELIMITER)
             indexes = dict()
+
             for i, line in enumerate(reader):
                 if i == 0:
-                    for j,heading in enumerate(line):
-                        if heading in variableNames:
-                            indexes[heading] = j
+                    for var in expandedVariablesNames:
+                        for j,heading in enumerate(line):
+                            if heading in expandedVariablesNames:
+                                indexes[heading] = j
 
-                    for heading in variableNames:
-                        if heading not in indexes.keys():
-                            indexes[heading] = -1
+                        for heading in expandedVariablesNames:
+                            if heading not in indexes.keys():
+                                indexes[heading] = -1
                 else:
                     values = dict()
                     for heading in indexes.keys():
@@ -48,7 +72,7 @@ with open(OUTPUT_CSV, 'w') as out:
                         else:
                             values[heading] = line[index]
                     
-                    for heading in variableNames:
+                    for heading in expandedVariablesNames:
                         out.write(values[heading])
                         out.write(",")
 
