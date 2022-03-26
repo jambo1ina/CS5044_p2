4450961384bdfbb13b6dee7b693d8c6343bf5a01
Jamie Munro <jbm@jbm.fyi>
2022-03-26 19:59:50 +0000

    Data processing pipeline

--- extract_data.py
+++ extract_data.py
@@ -1,80 +1,73 @@
 import csv
 
-INPUT_CSV = ["ess/round1.csv", "ess/round2.csv", "ess/round3.csv", "ess/round4.csv", "ess/round5.csv", "ess/round6.csv", "ess/round7.csv", "ess/round8.csv", "ess/round9.csv"]
-OUTPUT_CSV = "ess.csv"
-DELIMITER = ","
-VARIABLES_CSV = "variables.csv"
-VARIABLE_NAME_INDEX = 0
-MISSING_DATA = "-"
-
-#get variable names from input csv
-variableNames = []
-with open(VARIABLES_CSV, "r") as f:
-    reader = csv.reader(f, delimiter=DELIMITER)
-    for i, line in enumerate(reader):
-        if i == 0:
-            continue
-        else:
-            variableNames.append(line[VARIABLE_NAME_INDEX])
+def get_variables_names(variables_csv, delimiter=",", name_index=0):
+    variableNames = []
+    with open(variables_csv, "r") as f:
+        reader = csv.reader(f, delimiter=delimiter)
+        for i, line in enumerate(reader):
+            if i == 0:
+                continue
+            else:
+                variableNames.append(line[name_index])
+    return variableNames
 
 #expand all wildcard variable names
-expandedVariablesNames = []
-for variable in variableNames:
-    if "*" in variable:
-        prefix = variable[:variable.find("*")]
-        
-        for input in INPUT_CSV:
-            with open(input, "r") as f:
-               reader = csv.reader(f, delimiter=DELIMITER)
-               for i, line in enumerate(reader):
-                    if i != 0:
-                       break
-                    else:
-                        for heading in line:
-                            if heading.startswith(prefix) and heading not in expandedVariablesNames:
-                                expandedVariablesNames.append(heading)
+def expand_variable_names(input_csvs, variableNames, delimiter=","):
+    expandedVariablesNames = []
+    for variable in variableNames:
+        if "*" in variable:
+            prefix = variable[:variable.find("*")]
+            
+            for input in input_csvs:
+                with open(input, "r") as f:
+                    reader = csv.reader(f, delimiter=delimiter)
+                    for i, line in enumerate(reader):
+                            if i != 0:
+                                break
+                            else:
+                                for heading in line:
+                                    if heading.startswith(prefix) and heading not in expandedVariablesNames:
+                                        expandedVariablesNames.append(heading)
 
-    else:
-        expandedVariablesNames.append(variable)
-
-print("variables: ", variableNames)
-print("expanded: ", expandedVariablesNames)
-print("to file: ", OUTPUT_CSV)
+        else:
+            expandedVariablesNames.append(variable)
+    return expandedVariablesNames
 
 #genereate output CSV
-with open(OUTPUT_CSV, 'w') as out:
-    for heading in expandedVariablesNames:
-        out.write(heading)
-        out.write(",")
+def extract_data(input_csvs, output_csv, expandedVariablesNames, delimiter=",", missing_data="-"):
+    with open(output_csv, 'w') as out:
+        for heading in expandedVariablesNames:
+            out.write(heading)
+            out.write(",")
 
-    out.write("\n")
+        out.write("\n")
 
-    for file in INPUT_CSV:
-        with open(file, "r") as f:
-            reader = csv.reader(f, delimiter=DELIMITER)
-            indexes = dict()
+        for file in input_csvs:
+            with open(file, "r") as f:
+                reader = csv.reader(f, delimiter=delimiter)
+                indexes = dict()
 
-            for i, line in enumerate(reader):
-                if i == 0:
-                    for var in expandedVariablesNames:
-                        for j,heading in enumerate(line):
-                            if heading in expandedVariablesNames:
-                                indexes[heading] = j
+                for i, line in enumerate(reader):
+                    if i == 0:
+                        for var in expandedVariablesNames:
+                            for j,heading in enumerate(line):
+                                if heading in expandedVariablesNames:
+                                    indexes[heading] = j
 
+                            for heading in expandedVariablesNames:
+                                if heading not in indexes.keys():
+                                    indexes[heading] = -1
+                    else:
+                        values = dict()
+                        for heading in indexes.keys():
+                            index = indexes[heading]
+                            if index == -1:
+                                values[heading] = missing_data
+                            else:
+                                values[heading] = line[index]
+                        
                         for heading in expandedVariablesNames:
-                            if heading not in indexes.keys():
-                                indexes[heading] = -1
-                else:
-                    values = dict()
-                    for heading in indexes.keys():
-                        index = indexes[heading]
-                        if index == -1:
-                            values[heading] = MISSING_DATA
-                        else:
-                            values[heading] = line[index]
-                    
-                    for heading in expandedVariablesNames:
-                        out.write(values[heading])
-                        out.write(",")
+                            out.write(values[heading])
+                            out.write(",")
 
-                    out.write("\n")
+                        out.write("\n")
--- process_data.py
+++ process_data.py
@@ -1,76 +1,95 @@
 import csv
 import os
 
-from numpy import var
-
-INPUT_CSV = "ess.csv"
-INPUT_DIR = "mappings/"
-OUTPUT_CSV = "processed_data.csv"
-DELIMITER = ","
-MISSING_DATA = "-"
-
-VARIABLE_NAMES = ['essround', 'inwyys', 'inwyr', 'cntry', 'polintr', 'trstprl', 'vote', 'prtv*', 'lrscale', 'imwbcnt', 'edulvlb', 'emplrel', 'gndr', 'pdjobyr', 'hincfel', 'uemp12m', 'chldhhe']
-
 #get mappings
-mappings = dict()
+def load_mappings(input_dir, delimiter=","):
+    if input_dir[-1] != "/":
+        input_dir += "/"
 
-for dirName, subdirList, fileList in os.walk(INPUT_DIR):
-    round = dirName[dirName.rfind("/") + 1:]
-    if len(round) == 0:
-        continue
-    
-    mappings[round] = dict()
+    mappings = dict()
 
-    for file in fileList:
-        variableName = file[:file.rfind(".csv")]
-        mappings[round][variableName] = dict()
+    for dirName, _, fileList in os.walk(input_dir):
+        round = dirName[dirName.rfind("/") + 1:]
+        if len(round) == 0:
+            continue
+        
+        mappings[round] = dict()
 
-        with open(dirName + "/" + file, "r") as f:
-            reader = csv.reader(f, delimiter=DELIMITER)
-            for i, line in enumerate(reader):
-                if i == 0:
-                    mappings[round][variableName]["description"] = line[0]
-                else:
-                    mappings[round][variableName][line[0]] = line[1]
+        for file in fileList:
+            variableName = file[:file.rfind(".csv")]
+            mappings[round][variableName] = dict()
+
+            with open(dirName + "/" + file, "r") as f:
+                reader = csv.reader(f, delimiter=delimiter)
+                for i, line in enumerate(reader):
+                    if i == 0:
+                        mappings[round][variableName]["description"] = line[0]
+                    else:
+                        mappings[round][variableName][line[0]] = line[1]
+    return mappings
 
 #process data
-expandedVariableNames = []
-expansionMap = dict()
+def processData(input_csv, output_csv, variablesNames, mappings, delimiter=","):
+    expandedVariableNames = []
+    expansionMap = dict()
 
-with open(OUTPUT_CSV, "w") as out:
-    with open(INPUT_CSV, "r") as f:
-        reader = csv.reader(f, delimiter=DELIMITER)
-        for i, line in enumerate(reader):
-            if i == 0:
-                expandedVariableNames = line
+    with open(output_csv, "w") as out:
+        with open(input_csv, "r") as f:
+            reader = csv.reader(f, delimiter=delimiter)
+            for i, line in enumerate(reader):
+                if i == 0:
+                    expandedVariableNames = line
 
-                #generate expansion map
-                for expanded in expandedVariableNames:
-                    if expanded in VARIABLE_NAMES:
-                        continue
-                    else:
-                        for var in VARIABLE_NAMES:
-                            if "*" in var:
-                                prefix = var[:var.find("*")]
-                                if expanded.startswith(prefix):
-                                    expansionMap[expanded] = var
-
-                out.write(str(VARIABLE_NAMES).replace("[" , "").replace("]", "") + "\n")
-            else:
-                round = str(line[0])
-                for j,item in enumerate(line):
-                    if expandedVariableNames[j] in expansionMap:
-                        if item == "-" or item == "0":
+                    #generate expansion map
+                    for expanded in expandedVariableNames:
+                        if expanded in variablesNames:
                             continue
+                        else:
+                            for var in variablesNames:
+                                if "*" in var:
+                                    prefix = var[:var.find("*")]
+                                    if expanded.startswith(prefix):
+                                        expansionMap[expanded] = var
+
+                    # out.write(str(variablesNames).replace("[" , "").replace("]", "") + "\n")
+                    for heading in variablesNames:
+                        if heading == "":
+                            continue
+
+                        description = heading
+
+                        if "*" in heading:
+                            for expanded in expansionMap.keys():
+                                original = expansionMap[expanded]
+                                if original == heading:
+                                    heading = expanded
+                                    break
+
+                        for round in mappings.keys():
+                            if heading in mappings[round]:
+                                description = mappings[round][heading]["description"]
+                                break
+
+                        out.write(description + ",")
+                    out.write("\n")
+                else:
+                    round = str(line[0])
+                    for j,item in enumerate(line):
+                        if item == "":
+                            continue
+
+                        if expandedVariableNames[j] in expansionMap:
+                            if item == "-" or item == "0":
+                                continue
+                            else:
+                                if expandedVariableNames[j] in mappings[round] and item in mappings[round][expandedVariableNames[j]]:
+                                    out.write(mappings[round][expandedVariableNames[j]][item] + ",")
+                                else:
+                                    out.write(item + ",") 
                         else:
                             if expandedVariableNames[j] in mappings[round] and item in mappings[round][expandedVariableNames[j]]:
                                 out.write(mappings[round][expandedVariableNames[j]][item] + ",")
                             else:
-                                out.write(item + ",") 
-                    else:
-                        if expandedVariableNames[j] in mappings[round] and item in mappings[round][expandedVariableNames[j]]:
-                            out.write(mappings[round][expandedVariableNames[j]][item] + ",")
-                        else:
-                            out.write(item + ",")
+                                out.write(item + ",")
 
-                out.write("\n")
+                    out.write("\n")
